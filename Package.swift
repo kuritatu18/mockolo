30a2dc964452a91b4933e382025d41a3c919e6dd
Tatsunori Kuri <kuritatu18@gmail.com>
2020-08-28 17:51:41 +0900

    update swift version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 var dependencies: [Package.Dependency] = [
     .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.4")),
     .package(url: "https://github.com/apple/swift-tools-support-core.git", .exact("0.1.5")),
-    .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50200.0"))
+    .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .branch("master"))
 ]
 
 let package = Package(
